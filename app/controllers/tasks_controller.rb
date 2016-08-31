f868ffaf98d69ee347ccc1e3e0fdcb64465cdc22
Gabriella Castaneda <gabriellacastaneda@gmail.com>
2016-08-31 22:05:27 +0000

    added JS to add tasks to DOM

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 
     def index
-        render json: Task.all
+        render json: Task.all #renders JSON instead of HTML for all tasks in database
     end
 end
